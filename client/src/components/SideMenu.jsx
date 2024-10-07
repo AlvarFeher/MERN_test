2a547cdaeb3040ce569413448c6f36d539e211f8
alvarofeher <alvarofeherargullos@gmail.com>
2024-10-07 08:37:39 +0200

    styling

--- client/src/components/SideMenu.jsx
+++ client/src/components/SideMenu.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const SideMenu = () => {
   return (
     <div className="sideMenu">
-      <ul>
+      <ul className="menuList">
         <li><a href="#home">Home</a></li>
         <li><a href="#tasks">Tasks</a></li>
         <li><a href="#completed">Completed</a></li>
         <li><a href="#statistics">Statistics</a></li>
         <li><a href="#settings">Settings</a></li>
       </ul>
-      <button className="inline-flex items-center  whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3">Add new list</button>
+      <button className="addListBtn">Add new list</button>
     </div>
   );
 };
